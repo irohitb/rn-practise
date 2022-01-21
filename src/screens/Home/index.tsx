320a49dbd2930e26d1461b2d501a3ad44775ffde
irohitb <irohitbhatia@gmail.com>
2022-01-21 22:32:01 +0530

    feat: Added spinner overlay example

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,62 @@
-import React from 'react'
-import {View, Text } from "react-native";
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import Spinner from 'react-native-loading-spinner-overlay';
 
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu'
+});
 
-const HomeScreen = () => {
+type Props = {};
+export default class App extends Component<Props> {
+  state = {
+    spinner: true
+  };
 
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        spinner: !this.state.spinner
+      });
+    }, 3000);
+  }
+
+  render() {
     return (
-        <View>
-            <Text> Home Screen</Text>
-        </View>
-    )
+      <View style={styles.container}>
+        <Spinner
+          visible={this.state.spinner}
+          textContent={'Loading...'}
+          textStyle={styles.spinnerTextStyle}
+        />
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
 }
 
-export default HomeScreen
-
+const styles = StyleSheet.create({
+  spinnerTextStyle: {
+    color: '#FFF'
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5
+  }
+});
